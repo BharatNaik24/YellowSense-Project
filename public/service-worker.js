a6f21398a2404d4f1e68fd8872035729f91d17b8
BharatNaik24 <159719274+BharatNaik24@users.noreply.github.com>
2024-10-04 12:38:35 +0530

    PWA CHECK

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,11 @@ const CACHE_NAME = "job-swipe-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
+  "/logo192.png",
+  "/logo512.png",
   "/static/js/bundle.js",
   "/static/js/main.js",
+  "/styles.css",
   // Add any other assets you want to cache
 ];
 
@@ -19,6 +22,39 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return the response from the cached version
+      if (response) {
+        return response;
+      }
+      // Not in cache - return the result from the network
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
     })
   );
 });
