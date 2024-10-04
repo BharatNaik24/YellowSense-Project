2471726f3536a00fc1bfb0494d9fa932c5b48762
BharatNaik24 <159719274+BharatNaik24@users.noreply.github.com>
2024-10-04 13:57:49 +0530

    random check

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,13 +7,20 @@ const urlsToCache = [
   "/static/js/bundle.js",
   "/static/js/main.js",
   "/styles.css",
-  // Add any other assets you want to cache
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
